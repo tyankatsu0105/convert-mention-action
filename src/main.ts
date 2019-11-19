0130f56f614da129c7da112f3aaa8475e3ee3653
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2019-11-19 23:54:19 +0900

    ci(actions): test slack

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,43 @@
+// import * as github from '@actions/github';
 import * as core from '@actions/core';
-import { getPackageVersion } from './getPackageVersion';
+// import { Status } from './utils';
+// import { SectionBlock, MessageAttachment, MrkdwnElement } from '@slack/types';
+import {
+  IncomingWebhook,
+  IncomingWebhookSendArguments,
+  IncomingWebhookResult
+} from '@slack/webhook';
 
 async function run() {
   try {
-    const path = core.getInput('path');
+    const url = core.getInput('SLACK_WEBHOOK_URL');
+    const webhook = new IncomingWebhook(url);
 
-    core.debug(`Load package.json at ${path}`);
-
-    const result = getPackageVersion(path);
-
-    core.debug(`set output: version: ${result}`);
-    core.setOutput('version', result);
+    await webhook.send({
+      text: "I've got news for you..."
+    });
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-run();
+// run();
+
+// import * as core from '@actions/core';
+
+// async function run() {
+//   try {
+//     const path = core.getInput('path');
+
+//     core.debug(`Load package.json at ${path}`);
+
+//     const result = getPackageVersion(path);
+
+//     core.debug(`set output: version: ${result}`);
+//     core.setOutput('version', result);
+//   } catch (error) {
+//     core.setFailed(error.message);
+//   }
+// }
+
+// run();
