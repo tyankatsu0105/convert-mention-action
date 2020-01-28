dfd29ebbf1ed81cc0e65660f9d7df5efc8a58228
tyankatsu0105 <frips.ryilsufupe+dev@gmail.com>
2020-01-28 16:38:25 +0900

    improvement: imp

--- src/main.ts
+++ src/main.ts
@@ -9,22 +9,19 @@ const configPath = path.join(process.cwd(), './convert-mention.json');
 
 async function run() {
   try {
-    // const SLACK_WEBHOOK_URL: string = process.env.SLACK_WEBHOOK_URL || '';
-    // const SLACK_TOKEN: string = process.env.SLACK_TOKEN || '';
-
-    // /*eslint-disable-next-line @typescript-eslint/no-var-requires */
-    // const users = require(core.getInput('users'));
-
-    // const { context } = github;
-
-    // const slack = new Slack(SLACK_WEBHOOK_URL);
-
-    // const resultText = await generateResultText(users, SLACK_TOKEN, context);
-
-    // await slack.notify(resultText);
+    const SLACK_WEBHOOK_URL: string = process.env.SLACK_WEBHOOK_URL || '';
+    const SLACK_TOKEN: string = process.env.SLACK_TOKEN || '';
 
     /*eslint-disable-next-line @typescript-eslint/no-var-requires */
-    core.debug(require(core.getInput('users')));
+    const users = require(core.getInput('users'));
+
+    const { context } = github;
+
+    const slack = new Slack(SLACK_WEBHOOK_URL);
+
+    const resultText = await generateResultText(users, SLACK_TOKEN, context);
+
+    await slack.notify(resultText);
   } catch (error) {
     core.setFailed(error.message);
   }
